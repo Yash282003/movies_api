45330fb4b6c82b34bad5ab5009613b3ae56b8296
Yash282003 <114577553+Yash282003@users.noreply.github.com>
2023-08-24 20:13:15 +0530

    tested api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+const url = "https://www.omdbapi.com/?s=kabhi&apikey=a3787072";
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      setMovies(data.Search);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className='bg-purple-900'>
-      hiiiiiiiiiiiii
-    </div>
-  )
+    <>
+      {movies.map((movie) => {
+        return <img key={movie.imdbID} src={movie.Poster} alt={movie.Title} />;
+      })}
+    </>
+  );
 }
 
-export default App
+export default App;
