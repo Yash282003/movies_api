15556c2e3fa966473b1007dc17b717fb6e63eb9f
Yash282003 <114577553+Yash282003@users.noreply.github.com>
2023-10-02 11:34:06 +0530

    added new page for movieDetails

--- src/components/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage.jsx
@@ -1,21 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import dataContext from "../context/datacontext";
 
-const MovieDetailsPage = ({}) => {
-//   const { id } = useParams();
-  const {movies, setMovies} = useContext(dataContext);
+const MovieDetailsPage = () => {
+  const { id } = useParams();
+  const { movies , setMovies} = useContext(dataContext);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    // Find the movie based on its ID
+    const selectedMovie = movies.find(movie => movie.id.toString() === id);
+
+    if (selectedMovie) {
+      setMovieDetails(selectedMovie);
+    }
+  }, [id, movies]);
+
   return (
     <div>
-      {/* {movies ? (
+      {movieDetails ? (
         <div>
-       { movies.original_title}
-          
+          <h1>{movieDetails.original_title}</h1>
+          <p>{movieDetails.overview}</p>
+          <img
+            className="movie_poster"
+            src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
+            alt={movieDetails.original_title}
+          />
+          {/* Display other movie details as needed */}
         </div>
       ) : (
         <div>Loading...</div>
-      )} */}
-      hiii
+      )}
     </div>
   );
 };
